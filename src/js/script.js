319c3600fd11f55d86bbaa290a3a765d3bdc91f9
Alexey <dyremar92@gmail.com>
2020-07-28 10:35:06 +0300

    add validation

--- src/js/script.js
+++ src/js/script.js
@@ -50,4 +50,36 @@ $(document).ready(function(){
         })
     });
 
+    // Validate
+
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },    
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Минимум {0} символа!")
+                },
+                phone: "Неправильно введен номер телефона",
+                email: {
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Неправильно введен адрес почты",
+                }
+            }
+        });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
 });
